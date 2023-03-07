9463b376a2b4533ad900b56833f6b9af12be2186
M3zaque <mezackisilva@gmail.com>
2023-03-07 20:36:00 -0300

    lisa: bring up to voltage Signed-off-by: M3zaque <mezackisilva@gmail.com>

--- voltage_lisa.mk
+++ voltage_lisa.mk
@@ -12,24 +12,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
 # Inherit some common PixelOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-SUSHI_BOOTANIMATION := 1440
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1920
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_lisa
+PRODUCT_NAME := voltage_lisa
 PRODUCT_MODEL := Xiaomi 11 Lite NE
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Gapps
-WITH_GMS := true
-TARGET_CORE_GMS := true
-TARGET_GAPPS_ARCH := arm64
-
-# Rice Target
-TARGET_ENABLE_BLUR := true
-
-# Rice stuff
-RICE_OFFICIAL := true
+# Official-ify
+VOLTAGE_BUILD_TYPE := OFFICIAL
